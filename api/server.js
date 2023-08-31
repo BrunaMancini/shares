b915b0d59e46d5e5b09e0b74f6e3f2dfb20f596c
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-08-31 18:48:50 -0300

    updating server.js

--- api/server.js
+++ api/server.js
@@ -1,19 +1,19 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
+
+server.get('/api/sharesPortfolio', (req, res) => {
+    const db = router.db;
+    const data = db.get('api.sharesPortfolio').value();
+    res.json(data);
+  });
+
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
