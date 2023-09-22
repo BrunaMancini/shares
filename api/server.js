37af1b121dd787f461d2311ade2718d16fb6eb17
BrunaMancini <101213519+BrunaMancini@users.noreply.github.com>
2023-09-22 13:38:16 -0300

    updating routes

--- api/server.js
+++ api/server.js
@@ -5,15 +5,15 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 
-server.get('/api/sharesPortfolio', (req, res) => {
+server.get('/api/beneficiaryLTI/portfolio-consolidated', (req, res) => {
     const db = router.db;
-    const data = db.get('api.sharesPortfolio').value();
+    const data = db.get('api.beneficiaryLTI.portfolio-consolidated').value();
     res.json(data);
   });
 
-server.get('/api/sharesPrices', (req, res) => {
+server.get('/api/beneficiaryLTI/shares-by-ticker', (req, res) => {
     const db = router.db;
-    const data = db.get('api.sharesPrices').value();
+    const data = db.get('api.beneficiaryLTI.shares-by-ticker').value();
     res.json(data);
   });
 
